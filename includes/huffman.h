46698edee3bad1a975ea9a53413561055c48aeb6
Weikeup <boshun2009@gmail.com>
2022-03-10 22:52:05 +0800

    Define some functions and classes for  Huffman tree

--- includes/huffman.h
+++ includes/huffman.h
@@ -1,9 +1,33 @@
 #pragma once
+
+#include <iostream>
 #include "compressor.h"
 
-class Huffman : public Compressor{
+class Node {
 public:
+    string value;
+    unsigned int weight = 0;
+    Node *left = nullptr;
+    Node *right = nullptr;
+};
+
+class Huffman : public Compressor {
+private:
+    Node *tree = nullptr;
+
+    Node *releaseTree(Node *root);
+
+public:
+
+    Huffman();
+
+    ~Huffman();
+
     string compress(string &src) override;
 
     string decompress(string &src) override;
+
+    void readTree(basic_iostream<char> stream);
+
+    void writeTree(basic_iostream<char> stream);
 };
