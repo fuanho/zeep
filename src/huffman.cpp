aae8212fa20ab4dd9ecf109a334eb452463bea5f
Weikeup <boshun2009@gmail.com>
2022-03-10 22:53:37 +0800

    Implement a function used to release Huffman tree

--- src/huffman.cpp
+++ src/huffman.cpp
@@ -1,5 +1,21 @@
 #include "huffman.h"
 
+Huffman::Huffman() {
+    tree = new Node();
+}
+
+Huffman::~Huffman() {
+    releaseTree(tree);
+}
+
+Node *Huffman::releaseTree(Node *root) {
+    if (!root) return nullptr;
+    if (!root->left && !root->right) return root;
+    delete releaseTree(root->left);
+    delete releaseTree(root->right);
+    delete root;
+}
+
 string Huffman::compress(string &src) {
     return src;
 }
